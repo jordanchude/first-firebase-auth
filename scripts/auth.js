963b01dbd98b657407631f9b6316eb3ef63b8835
Jordan <jordan@chude.co>
2021-05-18 09:42:14 -0400

    sign in successful

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,1 +1,31 @@
 const signupForm = document.querySelector('#signup-form');
+
+// PREVENT AUTO-SUBMIT
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // GET USER INFO
+    const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
+
+    // SIGN UP USER
+    auth.createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+
+    // SIGNED IN
+    var user = userCredential.user;
+
+    })
+    .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+    })
+
+    // TO DO: EVENT LISTENER FOR LOGOUT?
+    // SIGN OUT
+    auth().signOut().then(() => {
+        // SIGN OUT SUCCESSFUL
+      }).catch((error) => {
+        // THROW ERROR
+      });
+});
